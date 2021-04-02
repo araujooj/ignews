32d84fbd4b936257c415ad1f8e7f948b91d5f8ff
Gabriel Araujo <gabriel@kenzie.com.br>
2021-04-02 09:57:38 -0300

    feature: create static site generation on home page

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+const SubscribeButton = () => (
+  <button className={styles.subscribeButton} type="button">
+    Subscribe now
+  </button>
+);
+
+export default SubscribeButton;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,67 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import SubscribeButton from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <main>
-        <section />
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏻 Hey, welcome </span>
+          <h1>
+            News about the
+            <span> React </span>
+            world.
+          </h1>
+          <p>
+            Get access to all the publications
+            <br />
+            <span>
+              for
+              {` ${product.amount} `}
+              month
+            </span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+// Utilizar getStaticProps apenas quando a página utiliza dados dinâmicos porém pode ser estática.
+// Ou seja... Dados que não mudam facilmente.
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IbO9YEX8A7yQBNbEw8wMBoc', {
+    expand: ['product'],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours,
+  };
+};
