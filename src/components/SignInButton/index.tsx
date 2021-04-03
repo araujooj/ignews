5d3eb1538f5f8d7072ee47fe7c483d55a554d57d
Gabriel Araujo <gabriel@kenzie.com.br>
2021-04-03 13:35:17 -0300

    feature: added creation of customer on faundbn

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 const SignInButton = () => {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button className={styles.signInButton} type="button">
       <FaGithub color="#04d361" />
-      araujooj
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button onClick={() => signIn('github')} className={styles.signInButton} type="button">
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,9 +1,37 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
-const SubscribeButton = () => (
-  <button className={styles.subscribeButton} type="button">
-    Subscribe now
-  </button>
-);
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      return signIn('github');
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+
+      stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <button onClick={handleSubscribe} className={styles.subscribeButton} type="button">
+      Subscribe now
+    </button>
+  );
+};
 
 export default SubscribeButton;
